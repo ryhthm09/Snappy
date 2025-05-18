f9bd0938bcef8b0e14ac63d16f1691b136336b17
ryhthm09 <anandsharma1123@gmail.com>
2025-05-18 06:56:42 +0530

    Added ADO Key Vault

--- server/index.js
+++ server/index.js
@@ -5,7 +5,16 @@ const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messagesRoutes");
 const socket = require("socket.io");
 
-const app = express()
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+const credential = new DefaultAzureCredential();
+const vaultName = 'kvsnappy';  
+const keyVaultUrl = `https://${vaultName}.vault.azure.net`;  
+const client = new SecretClient(keyVaultUrl, credential);
+
+console.log("Connected to Azure Key Vault");
+const app = express();
 require("dotenv").config();
 
 app.use(cors());
@@ -14,37 +23,48 @@ app.use(express.json());
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoutes);
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log("DB connection Successful");
-}).catch((err) => {
-  console.log(err.message);
-});
+(async () => {
+  try {
+    const MONGO_URL_SECRET = await client.getSecret("MONGO-URL");
+    const MONGO_URL = MONGO_URL_SECRET.value;
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB connection Successful");
+  } catch (err) {
+    console.log(err.message);
+  }
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server started on Port ${process.env.PORT}`);
-});
+  const PORT_SECRET = await client.getSecret("PORT");
+  const PORT = PORT_SECRET.value || 5000;
+  const CORS_ORIGIN_SECRET = await client.getSecret("CORS-ORIGIN");
+  const CORS_ORIGIN = CORS_ORIGIN_SECRET.value;
 
-const io = socket(server, {
-  cors: {
-    origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST"],
-    credentials:true,
-  },
-})
-global.onlineUsers = new Map();
-
-io.on("connection", (socket) => {
-  global.chatSocket = socket;
-  socket.on("add-user", (userId) => {
-    onlineUsers.set(userId, socket.id);
+  const server = app.listen(PORT, () => {
+    console.log(`Server started on Port ${PORT}`);
   });
-  socket.on("send-msg", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-receive", data.message);
-    }
+
+  const io = socket(server, {
+    cors: {
+      origin: CORS_ORIGIN,
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
   });
-});
+
+  global.onlineUsers = new Map();
+
+  io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+      onlineUsers.set(userId, socket.id);
+    });
+    socket.on("send-msg", (data) => {
+      const sendUserSocket = onlineUsers.get(data.to);
+      if (sendUserSocket) {
+        socket.to(sendUserSocket).emit("msg-receive", data.message);
+      }
+    });
+  });
+})();
