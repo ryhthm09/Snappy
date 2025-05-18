6e77db95fe0f1d95148e84a810c04049c4de6bd5
ryhthm09 <anandsharma1123@gmail.com>
2025-05-18 14:29:31 +0530

    Added the FE and BE endpoints

--- server/public/src/utils/APIRoutes.js
+++ server/public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://snappy-backend-bmecd6hfgeb2bjdj.westindia-01.azurewebsites.net";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
